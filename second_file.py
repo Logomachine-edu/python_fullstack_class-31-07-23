3237c831a3cc98201447e9cfff0dff384e1aa18d
Khasan Babakulov <khasanrustamovich@gmail.com>
2023-08-03 22:14:11 +0300

    2 developer started

--- second_file.py
+++ second_file.py
@@ -1,1 +1,7 @@
-# Тут будет мой второй код на Python
+# This program prints Hello, world!
+print("Hello, dog 1!") 
+print("Hello, dog 2!") 
+print("Hello, dog 3!") 
+print("Hello, dog 4!") 
+print("Hello, dog 5!") 
+ 
